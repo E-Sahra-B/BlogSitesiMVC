ddac501b7c6e1f5074d6c3c17dec9c3e09dc9f2d
E-Sahra-B <bendenizsahra@gmail.com>
2022-04-04 22:28:55 +0300

    ApiCrudIslemleri
    ApiCrudIslemleri

---
+++ BlogApi/Controllers/DefaultController.cs
@@ -0,0 +1,65 @@
+﻿using BlogApi.DataAccessLayer;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace BlogApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DefaultController : ControllerBase
+    {
+        [HttpGet("list")]
+        public IActionResult EmployeeList()
+        {
+            using var c = new ApiContext();
+            var values = c.Employees.ToList();
+            return Ok(values);
+        }
+        [HttpPost("add")]
+        public IActionResult EmployeeAdd(Employee employee)
+        {
+            using var c = new ApiContext();
+            c.Add(employee);
+            c.SaveChanges();
+            return Ok();
+        }
+        [HttpGet("getbyid{id}")]
+        public IActionResult EmployeeGet(int id)
+        {
+            using var c = new ApiContext();
+            var employee = c.Employees.Find(id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            return Ok(employee);
+        }
+        [HttpDelete("delete{id}")]
+        public IActionResult EmployeeDelete(int id)
+        {
+            using var c = new ApiContext();
+            var employee = c.Employees.Find(id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            c.Remove(employee);
+            c.SaveChanges();
+            return Ok();
+        }
+        [HttpPut("update")]
+        public IActionResult EmployeeUpdate(Employee employee)
+        {
+            using var c = new ApiContext();
+            var emp = c.Employees.Find(employee.Id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            emp.Name = employee.Name;
+            c.Update(emp);
+            c.SaveChanges();
+            return Ok();
+        }
+    }
+}
---
+++ BlogApi/DataAccessLayer/ApiContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BlogApi.DataAccessLayer
+{
+    public class ApiContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionBuilder)
+        {
+            optionBuilder.UseSqlServer("server=(localdb)\\MSSQLLocalDB;database=BlokApiContextDb; integrated security=true;");
+        }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
